9373a560ce1139be9bb842922f44494470c6f872
sugawani <nekorollykk@gmail.com>
2023-08-27 02:09:32 +0900

    fetch zenn articles

---
+++ src/fetch_zenn_articles.ts
@@ -0,0 +1,74 @@
+import { ZennArticle, Article, User } from "./types/zenn_article";
+import { cdate } from "cdate";
+
+const cdateJST = cdate().tz("Asia/Tokyo").cdateFn();
+
+interface UserArticles {
+    userName: User["name"];
+    userID: User["id"];
+    articles: Article[];
+}
+
+const extractLastMonthArticles = (articles: Article[]): Article[] => {
+    const lastMonth = cdateJST().add(-1, "month");
+    const firstDayOfLastMonth = lastMonth.startOf("month");
+    const lastDayOfLastMonth = lastMonth.endOf("month");
+
+    return articles.filter(article => {
+        const publishedAt = cdateJST(article.published_at);
+        return firstDayOfLastMonth <= publishedAt && publishedAt <= lastDayOfLastMonth
+    })
+}
+
+const reduceUserArticles = (articles: Article[]): UserArticles[] => {
+    return articles.reduce(
+        (acc: UserArticles[], article) => {
+            const foundUser = acc.find((user) => user.userID === article.user.id);
+            if (foundUser) {
+                foundUser.articles.push(article);
+                foundUser.articles.sort((a, b) =>
+                    a.published_at > b.published_at ? 1 : -1
+                );
+            } else {
+                acc.push({
+                    userID: article.user.id,
+                    userName: article.user.name,
+                    articles: [article],
+                });
+            }
+            return acc;
+        },
+        [],
+    );
+}
+
+function makeMessage(userArticles: UserArticles[]): string {
+    let message = "";
+    userArticles.forEach((userArticle, i) => {
+        let m = `${userArticle.userName} さんの先月のZennブログ投稿です\n`;
+        userArticle.articles.forEach((article) => {
+            m += `・ ${article.title} | https://zenn.dev${article.path} | ${cdateJST(article.published_at).format("YYYY-MM-DD")
+                }\n`;
+        });
+        message += m;
+        if (userArticles.length - 1 !== i) {
+            message += `\n`;
+        }
+    });
+    if (message === "") {
+        `先月のZennブログ投稿はありませんでした… :cry:`;
+    }
+    return message;
+}
+
+export const fetchZennArticleMessage = async (publicationName: string): Promise<string> => {
+    const url = `https://zenn.dev/api/articles?publication_name=${publicationName}&count=20&order=latest`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw `failed to fetch articles err: ${response.body}`;
+    }
+    const data: ZennArticle = await response.json();
+    const lastMonthArticles = extractLastMonthArticles(data.articles);
+    const userArticles = reduceUserArticles(lastMonthArticles);
+    return makeMessage(userArticles);
+}
--- src/worker.ts
+++ src/worker.ts
@@ -1,17 +1,20 @@
 import { SlackApp, SlackEdgeAppEnv } from "slack-cloudflare-workers";
+import { fetchZennArticleMessage } from "./fetch_zenn_articles";
 
 export interface Env extends SlackEdgeAppEnv {
 	POST_CHANNEL_ID: string;
+	PUBLICATION_NAME: string;
 }
 
 export default {
 	// The scheduled handler is invoked at the interval set in our wrangler.toml's
 	// [[triggers]] configuration.
-	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
+	async scheduled(_event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
 		const app = new SlackApp({ env });
-		app.client.chat.postMessage({
-			channel: env.POST_CHANNEL_ID,
-			text: "test message"
-		});
+		ctx.waitUntil(
+			app.client.chat.postMessage({
+				channel: env.POST_CHANNEL_ID,
+				text: await fetchZennArticleMessage(env.PUBLICATION_NAME)
+			}));
 	},
 };
